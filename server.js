dd6a3f3ccca7ff07bca08d854b0f52a8427067bf
lockdown6 <38168894+lockdown6@users.noreply.github.com>
2019-01-06 08:43:40 +0200

    1.3

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
+const cors = require('cors');
+
 
 const app = express();
 
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 MongoClient.connect(db.url, (err, client) => {
   if (err) return console.log(err)
