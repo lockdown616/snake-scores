e03086b9fb589867a6073d3647539582064f4748
lockdown6 <38168894+lockdown6@users.noreply.github.com>
2019-01-06 08:59:55 +0200

    1.5

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
